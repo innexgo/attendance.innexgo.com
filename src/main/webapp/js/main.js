9bba747a39bdd0e56383395c6c36c1c389164c51
pimpale <gpimpale29@gmail.com>
2018-12-03 17:20:43 -0800

    added shinier webpage

--- src/main/webapp/js/main.js
+++ src/main/webapp/js/main.js
@@ -1,27 +1,63 @@
 function thisUrl(){
-	return window.location;
+				return window.location;
 }
 
 function baseUrl(url) {
-	return url.protocol + "//" + url.host + "/" + url.pathname.split('/')[1];
+				return url.protocol + "//" + url.host + "/" + url.pathname.split('/')[1];
 }
 
 function expandStudent(id) {
-	var url = baseUrl(thisUrl())+"students/"+id+"/";
-	var request = new XMLHttpRequest();
-	request.open('GET', url, true);
-	request.onload = function () {
-		if (request.status >= 200 && request.status < 400) {
-			var data = JSON.parse(this.response);
-			document.getElementById('studentdisplay').innerHTML = 
-				'<p>Student ID: ' + data.id + '</p>' + 
-				'<p>Name: ' + data.name + '</p>';
-		} else {
-			console.log('error');
-		}
-	}
-	request.send();
+				var url = baseUrl(thisUrl())+"students/"+id+"/";
+				var request = new XMLHttpRequest();
+				request.open('GET', url, true);
+				request.onload = function () {
+								if (request.status >= 200 && request.status < 400) {
+												var data = JSON.parse(this.response);
+
+								} else {
+												console.log('error');
+								}
+				}
+				request.send();
 }
 
-expandStudent(1);
 
+function addHistoryTableEntry(name, roomNumber, timestamp)
+{
+				document.getElementById("history-table").innerHTML =
+								'<tr>' + 
+								'<td><i class="fa fa-sign-in text-red large"></i></td>' +
+								'<td class="yellow">' +
+								'<strong>' + name + '</strong>' +
+								'signed-in to Room' +
+								'<strong>' + roomNumber + '</strong>' + 
+								'on' + date + 
+								'</td> + 
+								'<td><i>5 secs ago</i></td>' + 
+								'</tr>';
+}
+
+
+
+// Get the Sidebar
+var mySidebar = document.getElementById("mySidebar");
+
+// Get the DIV with overlay effect
+var overlayBg = document.getElementById("myOverlay");
+
+// Toggle between showing and hiding the sidebar, and add overlay effect
+function w3_open() {
+				if (mySidebar.style.display === 'block') {
+								mySidebar.style.display = 'none';
+								overlayBg.style.display = "none";
+				} else {
+								mySidebar.style.display = 'block';
+								overlayBg.style.display = "block";
+				}
+}
+
+// Close the sidebar with the close button
+function w3_close() {
+				mySidebar.style.display = "none";
+				overlayBg.style.display = "none";
+}
