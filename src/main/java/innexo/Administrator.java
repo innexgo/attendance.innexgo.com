b3b6b0cfca64eac36aa8c4dc3ca535f1cec0a104
pimpale <gpimpale29@gmail.com>
2019-04-12 02:09:26 -0700

    no security version that works

---
+++ src/main/java/innexo/Administrator.java
@@ -0,0 +1,5 @@
+package innexo;
+
+public class Administrator {
+	
+}
--- src/main/webapp/js/common.js
+++ src/main/webapp/js/common.js
@@ -1,5 +1,24 @@
 "use strict"
 
+function setCookie(cname, cval) {
+  document.cookie = cname + "=" + cvalue + "; path=/";
+}
+
+function getCookie(cname) {
+  var name = cname + "=";
+  var ca = document.cookie.split(';');
+  for(var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
 function isWhitespace(str) {
   return !(/\S/.test(str));
 }
