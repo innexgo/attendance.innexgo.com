42c02420c3d9cab59140520158dd85cb4ac3a802
ProbablyanAsian <43358667+probablyanasian@users.noreply.github.com>
2019-09-05 07:31:24 -0700

    alerts now disappear after 3 sec

--- src/main/webapp/js/common.js
+++ src/main/webapp/js/common.js
@@ -1,29 +1,29 @@
 "use strict"
 
 function isEmpty(str) {
-    return (!str || 0 === str.length);
+  return (!str || 0 === str.length);
 }
 
-function thisUrl(){
-  return window.location.protocol  + "//" + window.location.host;
+function thisUrl() {
+  return window.location.protocol + "//" + window.location.host;
 }
 
 function escapeHtml(unsafe) {
-    return unsafe
-         .replace(/&/g, "&amp;")
-         .replace(/</g, "&lt;")
-         .replace(/>/g, "&gt;")
-         .replace(/"/g, "&quot;")
-         .replace(/'/g, "&#039;");
- }
+  return unsafe
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/"/g, "&quot;")
+    .replace(/'/g, "&#039;");
+}
 
 function request(url, functionOnLoad, functionOnError) {
   var xhr = new XMLHttpRequest();
   xhr.open('GET', url, true);
-  xhr.onload = function() {
+  xhr.onload = function () {
     if (xhr.readyState == 4 && xhr.status == 200) {
       functionOnLoad(xhr);
-    } else if(xhr.readyState == 4 && xhr.status != 200) {
+    } else if (xhr.readyState == 4 && xhr.status != 200) {
       functionOnError(xhr);
     }
   };
@@ -31,34 +31,37 @@ function request(url, functionOnLoad, functionOnError) {
 }
 
 function ordinal_suffix_of(i) {
-    var j = i % 10,
-        k = i % 100;
-    if (j == 1 && k != 11) {
-        return i + "st";
-    }
-    if (j == 2 && k != 12) {
-        return i + "nd";
-    }
-    if (j == 3 && k != 13) {
-        return i + "rd";
-    }
-    return i + "th";
+  var j = i % 10,
+    k = i % 100;
+  if (j == 1 && k != 11) {
+    return i + "st";
+  }
+  if (j == 2 && k != 12) {
+    return i + "nd";
+  }
+  if (j == 3 && k != 13) {
+    return i + "rd";
+  }
+  return i + "th";
 }
 
 function giveAlert(innerHTML, type) {
   document.getElementById('alert-zone').innerHTML +=
-  '<div class="alert alert-dismissable ' + type + '" role="alert">' +
+    '<div class="alert alert-dismissable ' + type + '" role="alert">' +
     innerHTML +
     '<button type="button" class="close" data-dismiss="alert" aria-label="Close">' +
-      '<span aria-hidden="true">&times;</span>' +
+    '<span aria-hidden="true">&times;</span>' +
     '</button>' +
-  '</div>';
+    '</div>';
+  $(".alert").fadeTo(3000, 500).slideUp(500, function () {
+    $(".alert").slideUp(500);
+  });
 }
 
 function toGraduatingYear(grade) {
   var currentTime = moment();
   var currentYear = currentTime.year();
-  if(currentTime.month() >= 7) {
+  if (currentTime.month() >= 7) {
     currentYear++;
   }
   return currentYear;
