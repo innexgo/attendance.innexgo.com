98af8e02c8df5610ad6ab91572fc830a801ba562
ProbablyanAsian <43358667+probablyanasian@users.noreply.github.com>
2019-09-06 06:58:35 -0700

    Check for blanks, no double tr.

--- src/main/webapp/js/currentstatus.js
+++ src/main/webapp/js/currentstatus.js
@@ -6,9 +6,9 @@ function currentStatus() {
   var table = document.getElementById('current-status-table');
 
   //bail if we dont have all of the necessary cookies
-  if(apiKey == null || course == null || period == null) {
+  if (apiKey == null || course == null || period == null) {
     console.log('lack necessary cookies to calculate current status');
-    table.innerHTML='';
+    table.innerHTML = '';
     return;
   }
 
@@ -16,68 +16,66 @@ function currentStatus() {
   request(thisUrl() + '/student/' +
     '?courseId=' + course.id +
     '&apiKey=' + apiKey.key,
-    function(xhr) {
+    function (xhr) {
       var students = JSON.parse(xhr.responseText);
       // get irregularities
       request(thisUrl() + '/irregularity/' +
         '?courseId=' + course.id +
         '&periodId=' + period.id +
         '&apiKey=' + apiKey.key,
-        function(xhr) {
+        function (xhr) {
           var irregularities = JSON.parse(xhr.responseText).sort((a, b) => (a.time > b.time) ? 1 : -1);
 
           //blank table
-          table.innerHTML='';
+          table.innerHTML = '';
 
-          for(var i = 0; i < students.length; i++) {
-            var text =  '<span class="fa fa-check"></span>'
+          for (var i = 0; i < students.length; i++) {
+            var text = '<span class="fa fa-check"></span>'
             var bgcolor = '#ccffcc';
             var fgcolor = '#00ff00';
             var student = students[i];
 
             var irregularity = irregularities.filter(i => i.student.id == student.id).pop();
             var type = irregularity == null ? null : irregularity.type;
-            if (type  == 'absent') {
-              text =  '<span class="fa fa-times"></span>'
+            if (type == 'absent') {
+              text = '<span class="fa fa-times"></span>'
               bgcolor = '#ffcccc';
               fgcolor = '#ff0000';
             } else if (type == 'tardy') {
-              text =  '<span class="fa fa-check"></span>'
+              text = '<span class="fa fa-check"></span>'
               bgcolor = '#ffffcc';
               fgcolor = '#ffff00';
             } else if (type == 'left_early') {
-              text =  '<span class="fa fa-sign-out-alt"></span>'
+              text = '<span class="fa fa-sign-out-alt"></span>'
               bgcolor = '#ccffff';
               fgcolor = '#00ffff';
             } else if (type == 'left_temporarily') {
-              text =  '<span class="fa fa-check"></span>'
+              text = '<span class="fa fa-check"></span>'
               bgcolor = '#ccffff';
               fgcolor = '#00ffff';
             }
 
             // put values in table
-            table.insertRow(0).innerHTML=
-              ('<tr>' +
-                '<td>' + student.name + '</td>' +
-                '<td>' + student.id + '</td>' +
-                '<td style="background-color:'+bgcolor+';color:'+fgcolor+'">' + text + '</td>' +
-                '</tr>');
+            table.insertRow(0).innerHTML =
+              ('<td>' + student.name + '</td>' +
+               '<td>' + student.id + '</td>' +
+               '<td style="background-color:' + bgcolor + ';color:' + fgcolor + '">' + text + '</td>')
           }
         },
         //failure
-        function(xhr) {
+        function (xhr) {
           return;
         }
       );
     },
     //failure
-    function(xhr) {
+    function (xhr) {
       return;
     }
   );
 }
 
-$(document).ready(function() {
+$(document).ready(function () {
   currentStatus();
   setInterval(currentStatus, 5000);
 })
