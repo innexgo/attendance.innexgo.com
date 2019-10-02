388cdc8e0497e10088ab0dda9e4243388b2b6037
ProbablyanAsian <43358667+probablyanasian@users.noreply.github.com>
2019-10-01 17:51:40 -0700

    Userprofile rework

--- src/main/webapp/js/userprofile.js
+++ src/main/webapp/js/userprofile.js
@@ -1,6 +1,6 @@
 "use strict"
 
-async function loadData() {
+function loadData() {
   var apiKey = Cookies.getJSON('apiKey');
   var searchParams = new URLSearchParams(window.location.search);
 
@@ -14,7 +14,6 @@ async function loadData() {
     0:'Administrator',
     1:'Teacher'
   }
-  var userPosition;
   request(thisUrl() + '/user/' +
     '?userId=' + userId +
     '&apiKey=' + apiKey.key,
@@ -23,7 +22,10 @@ async function loadData() {
       document.getElementById('user-name').innerHTML = userResponse.name;
       document.getElementById('user-email').innerHTML = 'Email: ' + userResponse.email;
       document.getElementById('user-position').innerHTML = position[userResponse.ring];
-      userPosition = userResponse.ring;
+      if (userResponse.ring == 0){
+      var element = document.getElementById('user-courses-table');
+      element.parentNode.removeChild(element);
+  }
     },
     function (xhr) {
       //failure
@@ -31,51 +33,41 @@ async function loadData() {
       return
     }
   );
-  if (userPosition === undefined) {
-    await sleep(500);
-  }
-
-  if (userPosition == 0){
-    var element = document.getElementById('user-courses-table');
-    element.parentNode.removeChild(element);
-  }
-  else {
-    request(thisUrl() + '/course/' +
-      '?teacherId=' + userId +
-      '&apiKey=' + apiKey.key,
-      function (xhr) {
-        var teacherCourses = JSON.parse(xhr.responseText);
-        var coursePeriods = [];
-        for (var p = 1; p <= 7; p++) {
-          var currentPeriodList = teacherCourses.filter(c => c.period == p);
-          coursePeriods.push(currentPeriodList.length == 0 ? null : currentPeriodList[0])
-        }
-        var classTable = document.getElementById('user-courses');
-        console.log(coursePeriods);
-        coursePeriods.sort(function (a, b) {
-          if (a != null && b != null) {
-            return b.period-a.period
-          } else {
-            return -1
-          };
-        });
-        coursePeriods.forEach(function (course) {
-          if (course != null) {
-            var newrow = classTable.insertRow(0);
-            newrow.innerHTML =
-              ('<td>' + course.period + '</td>' +
-                '<td>' + course.subject + '</td>' +
-                '<td>' + course.location.name + '</td>');
-          }
-        });
-      },
-      function (xhr) {
-        //failure
-        giveAlert('Failed to connect to server.', 'alert-danger');
-        return;
+  
+  request(thisUrl() + '/course/' +
+    '?teacherId=' + userId +
+    '&apiKey=' + apiKey.key,
+    function (xhr) {
+      var teacherCourses = JSON.parse(xhr.responseText);
+      var coursePeriods = [];
+      for (var p = 1; p <= 7; p++) {
+        var currentPeriodList = teacherCourses.filter(c => c.period == p);
+        coursePeriods.push(currentPeriodList.length == 0 ? null : currentPeriodList[0])
       }
-    );
-  }
+      var classTable = document.getElementById('user-courses');
+      coursePeriods.sort(function (a, b) {
+        if (a != null && b != null) {
+          return b.period-a.period
+        } else {
+          return -1
+        };
+      });
+      coursePeriods.forEach(function (course) {
+        if (course != null) {
+          var newrow = classTable.insertRow(0);
+          newrow.innerHTML =
+            ('<td>' + course.period + '</td>' +
+              '<td>' + course.subject + '</td>' +
+              '<td>' + course.location.name + '</td>');
+        }
+      });
+    },
+    function (xhr) {
+      //failure
+      giveAlert('Failed to connect to server.', 'alert-danger');
+      return;
+    }
+  );
 };
 
 $(document).ready(function() {
