68864fe5d4fed40c256309b8a584bc2d10829324
pimpale <gpimpale29@gmail.com>
2019-11-11 00:33:47 +0000

    some changes for https

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,7 @@
-#!/bin/sh
-
-java -jar -Dserver.port=80 ./build/libs/innexgo-0.0.1-SNAPSHOT.jar > ~/logs.txt &
+#!/bin/
+java -jar \
+	-Dserver.port=443 \
+	-Dserver.ssl.enabled=true \
+	-Dserver.ssl.key=/etc/letsencrypt/q \
+	-Dserver.ssl.enabled=true \
+	./build/libs/innexgo-0.0.1-SNAPSHOT.jar > ~/logs.txt &
--- src/main/java/innexgo/InnexgoApplication.java
+++ src/main/java/innexgo/InnexgoApplication.java
@@ -13,14 +13,40 @@ import org.springframework.context.annotation.Configuration;
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class InnexgoApplication {
 
-  @Bean
-  public ThreadPoolTaskScheduler threadPoolTaskScheduler() {
-      ThreadPoolTaskScheduler threadPoolTaskScheduler = new ThreadPoolTaskScheduler();
-      threadPoolTaskScheduler.setPoolSize(2); // We need 2 threads to execute the midnight sign out and the absence insertion
-      return threadPoolTaskScheduler;
-  }
+	@Bean
+	public ThreadPoolTaskScheduler threadPoolTaskScheduler() {
+		ThreadPoolTaskScheduler threadPoolTaskScheduler = new ThreadPoolTaskScheduler();
+		threadPoolTaskScheduler.setPoolSize(2); // We need 2 threads to execute the midnight sign out and the absence insertion
+		return threadPoolTaskScheduler;
+	}
 
-  public static void main(String[] args) {
-    SpringApplication.run(InnexgoApplication.class, args);
-  }
+	@Bean
+	public ServletWebServerFactory servletContainer() {
+		TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
+			@Override
+			protected void postProcessContext(Context context) {
+				SecurityConstraint securityConstraint = new SecurityConstraint();
+				securityConstraint.setUserConstraint("CONFIDENTIAL");
+				SecurityCollection collection = new SecurityCollection();
+				collection.addPattern("/*");
+				securityConstraint.addCollection(collection);
+				context.addConstraint(securityConstraint);
+			}
+		};
+		tomcat.addAdditionalTomcatConnectors(redirectConnector());
+		return tomcat;
+	}
+
+	private Connector redirectConnector() {
+		Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+		connector.setScheme("http");
+		connector.setPort(8080);
+		connector.setSecure(false);
+		connector.setRedirectPort(8443);
+		return connector;
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(InnexgoApplication.class, args);
+	}
 }
