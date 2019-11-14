9ca52ce7459c239e8236ea8e66900aa75dab86c9
pimpale <gpimpale29@gmail.com>
2019-11-14 03:54:56 +0000

    very spicy changes to deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,15 @@
-#!/bin/
-java -jar \
+#!/bin/bash
+
+set -e
+
+echo Please enter password for innexgo.com key below:
+read -s PASSWORD
+
+sudo java -jar \
 	-Dserver.port=443 \
 	-Dserver.ssl.enabled=true \
-	-Dserver.ssl.key=/etc/letsencrypt/q \
-	-Dserver.ssl.enabled=true \
+	-Dserver.ssl.key-store=/etc/letsencrypt/live/innexgo.com/keystore.p12 \
+	-Dserver.ssl.key-store-password=$PASSWORD \
+	-Dserver.ssl.keyStoreType=PKCS12 \
+	-Dserver.ssl.keyAlias=tomcat \
 	./build/libs/innexgo-0.0.1-SNAPSHOT.jar > ~/logs.txt &
