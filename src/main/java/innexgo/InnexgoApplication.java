cc7f7ec190f8ea7a823b668c3f5c0057797c547d
gpi <gpimpale29@gmail.com>
2020-01-16 16:16:36 -0800

    deleted beans... run another service when you deploy ssl

--- src/main/java/innexgo/InnexgoApplication.java
+++ src/main/java/innexgo/InnexgoApplication.java
@@ -39,38 +39,12 @@ import org.apache.tomcat.util.descriptor.web.SecurityCollection;
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class InnexgoApplication {
 
+  // We need 2 threads to execute the midnight sign out and the absence insertion
   @Bean
   public ThreadPoolTaskScheduler threadPoolTaskScheduler() {
     ThreadPoolTaskScheduler threadPoolTaskScheduler = new ThreadPoolTaskScheduler();
-    threadPoolTaskScheduler.setPoolSize(2); // We need 2 threads to execute the midnight sign out and the absence insertion
+    threadPoolTaskScheduler.setPoolSize(2);
     return threadPoolTaskScheduler;
-  }
-
-  /* TO REDIRECT ALL TO HTTPS */
-  @Bean
-  public ServletWebServerFactory servletContainer() {
-    TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
-      @Override
-      protected void postProcessContext(Context context) {
-        SecurityConstraint securityConstraint = new SecurityConstraint();
-        securityConstraint.setUserConstraint("CONFIDENTIAL");
-        SecurityCollection collection = new SecurityCollection();
-        collection.addPattern("/*");
-        securityConstraint.addCollection(collection);
-        context.addConstraint(securityConstraint);
-      }
-    };
-    tomcat.addAdditionalTomcatConnectors(redirectConnector());
-    return tomcat;
-  }
-
-  private Connector redirectConnector() {
-    Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
-    connector.setScheme("http");
-    connector.setPort(8080);
-    connector.setSecure(false);
-    connector.setRedirectPort(8443);
-    return connector;
   }
 
   public static void main(String[] args) {
