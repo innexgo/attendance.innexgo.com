c0b76d3ca725b2dd1ffee7dc6fa1915ea1483170
marekpinto <marek.pinto@gmail.com>
2020-02-08 11:59:17 -0800

    JS for PDFS

---
+++ src/main/webapp/js/studentpdf.js
@@ -0,0 +1,18 @@
+var fonts = {
+	Roboto: {
+		normal: 'fonts/Roboto-Regular.ttf',
+		bold: 'fonts/Roboto-Medium.ttf',
+		italics: 'fonts/Roboto-Italic.ttf',
+		bolditalics: 'fonts/Roboto-MediumItalic.ttf'
+	}
+};
+
+var docDefinition = {
+	content: [
+		'First paragraph',
+		'Another paragraph, this time a little bit longer to make sure, this line will be divided into at least two lines'
+	]
+};
+
+pdfMake.createPdf(docDefinition).open();
+
