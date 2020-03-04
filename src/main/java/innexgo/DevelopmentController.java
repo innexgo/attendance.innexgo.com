91f36e4a58643d61ba888fb2031cef281182c5ce
gpi <gpimpale29@gmail.com>
2020-03-03 18:29:15 -0800

    added corresponding methods in devcontroller

--- src/main/java/innexgo/DevelopmentController.java
+++ src/main/java/innexgo/DevelopmentController.java
@@ -18,16 +18,20 @@
 
 package innexgo;
 
-import java.time.*;
-import java.util.*;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.ZonedDateTime;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = {"/dev/"})
+@RequestMapping(value = { "/dev/" })
 public class DevelopmentController {
 
   @Autowired ApiKeyService apiKeyService;
@@ -43,11 +47,11 @@ public class DevelopmentController {
   @Autowired StudentService studentService;
   @Autowired UserService userService;
 
-  @Autowired InnexgoService innexgoService;
+  @Autowired
+  InnexgoService innexgoService;
 
   static final ResponseEntity<?> BAD_REQUEST = new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-  static final ResponseEntity<?> INTERNAL_SERVER_ERROR =
-      new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+  static final ResponseEntity<?> INTERNAL_SERVER_ERROR = new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
   static final ResponseEntity<?> OK = new ResponseEntity<>(HttpStatus.OK);
   static final ResponseEntity<?> UNAUTHORIZED = new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
   static final ResponseEntity<?> NOT_FOUND = new ResponseEntity<>(HttpStatus.NOT_FOUND);
@@ -60,9 +64,15 @@ public class DevelopmentController {
     return OK;
   }
 
+  @RequestMapping("/issueAbsences/")
+  public ResponseEntity<?> issueAbsences(@RequestParam("periodStartTime") Long periodStartTime) {
+    innexgoService.issueAbsences(periodStartTime);
+    return OK;
+  }
+
   @RequestMapping("/populateUsers/")
   public ResponseEntity<?> populateUsers() {
-    if(userService.getAll().size() == 0) {
+    if (userService.getAll().size() == 0) {
       User user = new User();
       user.name = "root";
       user.email = rootEmail;
@@ -76,7 +86,7 @@ public class DevelopmentController {
 
   @RequestMapping("/lmaoRootKey/")
   public ResponseEntity<?> lmaoRootKey() {
-    if(userService.existsByEmail(rootEmail)) {
+    if (userService.existsByEmail(rootEmail)) {
       User u = userService.getByEmail(rootEmail);
 
       ApiKey apiKey = new ApiKey();
@@ -95,10 +105,10 @@ public class DevelopmentController {
   @RequestMapping("/populateTestingPeriods/")
   public ResponseEntity<?> populateTestingPeriods() {
     long minute = 1 * 60 * 1000;
-    long initialTime = System.currentTimeMillis() - 3*minute;
+    long initialTime = System.currentTimeMillis() - 3 * minute;
     for (int i = 1; i < 7; i++) {
       Period period = new Period();
-      period.startTime = initialTime + minute*i*3;
+      period.startTime = initialTime + minute * i * 3;
       period.number = i;
       period.type = Period.CLASS_PERIOD;
       period.temp = true;
@@ -110,35 +120,34 @@ public class DevelopmentController {
   // deletes periods with a length of less than 4 min
   @RequestMapping("/deleteTestingPeriods/")
   public ResponseEntity<?> deleteTestingPeriods() {
-    List<Period> periodList = periodService.query(
-      null,          // Long startTime,
-      null,          // Long number,
-      null,          // String type,
-      null,          // Long minStartTime,
-      null,          // Long maxStartTime,
-      true,          // Boolean temp,
-      0,             // long offset,
-      Long.MAX_VALUE // long count
-      );
+    List<Period> periodList = periodService.query(null, // Long startTime,
+        null, // Long number,
+        null, // String type,
+        null, // Long minStartTime,
+        null, // Long maxStartTime,
+        true, // Boolean temp,
+        0, // long offset,
+        Long.MAX_VALUE // long count
+    );
 
-    for(Period currentPeriod : periodList) {
+    for (Period currentPeriod : periodList) {
       // delete irregularities, as they reference periods
       List<Irregularity> irregularities = irregularityService.query(
 
-          null,                    // Long id,
-          null,                    // Long studentId,
-          null,                    // Long courseId,
+          null, // Long id,
+          null, // Long studentId,
+          null, // Long courseId,
           currentPeriod.startTime, // Long periodStartTime,
-          null,                    // Long teacherId,
-          null,                    // String type,
-          null,                    // Long time,
-          null,                    // Long minTime,
-          null,                    // Long maxTime,
-          0,                       // long offset
-          Long.MAX_VALUE           // long count
-        );
+          null, // Long teacherId,
+          null, // String type,
+          null, // Long time,
+          null, // Long minTime,
+          null, // Long maxTime,
+          0, // long offset
+          Long.MAX_VALUE // long count
+      );
 
-      for(Irregularity irregularity  : irregularities) {
+      for (Irregularity irregularity : irregularities) {
         irregularityService.deleteById(irregularity.id);
       }
       periodService.deleteByStartTime(currentPeriod.startTime);
@@ -149,8 +158,7 @@ public class DevelopmentController {
   @RequestMapping("/populatePeriods/")
   public ResponseEntity<?> populatePeriods() {
     periodService.deleteAll();
-    LocalDate sunday =
-        ZonedDateTime.now(Utils.TIMEZONE).toLocalDate().plusWeeks(-1).with(DayOfWeek.SUNDAY);
+    LocalDate sunday = ZonedDateTime.now(Utils.TIMEZONE).toLocalDate().plusWeeks(-1).with(DayOfWeek.SUNDAY);
 
     // get weekdays
     LocalDate monday = sunday.plusDays(1);
@@ -163,83 +171,82 @@ public class DevelopmentController {
       // collab
       LocalDate thisMonday = monday.plusWeeks(week);
 
-      addPeriod(thisMonday, 101, Period.PASSING_PERIOD,  "7:10");
-      addPeriod(thisMonday, 1,   Period.CLASS_PERIOD,    "7:15");
-      addPeriod(thisMonday, 102, Period.PASSING_PERIOD,  "7:55");
-      addPeriod(thisMonday, 2,   Period.CLASS_PERIOD,    "8:00");
-      addPeriod(thisMonday, 103, Period.PASSING_PERIOD,  "8:40");
-      addPeriod(thisMonday, 3,   Period.CLASS_PERIOD,    "8:45");
-      addPeriod(thisMonday, 204, Period.BREAK_PERIOD,    "9:25");
-      addPeriod(thisMonday, 104, Period.PASSING_PERIOD,  "9:40");
-      addPeriod(thisMonday, 4,   Period.CLASS_PERIOD,    "9:45");
+      addPeriod(thisMonday, 101, Period.PASSING_PERIOD, "7:10");
+      addPeriod(thisMonday, 1, Period.CLASS_PERIOD, "7:15");
+      addPeriod(thisMonday, 102, Period.PASSING_PERIOD, "7:55");
+      addPeriod(thisMonday, 2, Period.CLASS_PERIOD, "8:00");
+      addPeriod(thisMonday, 103, Period.PASSING_PERIOD, "8:40");
+      addPeriod(thisMonday, 3, Period.CLASS_PERIOD, "8:45");
+      addPeriod(thisMonday, 204, Period.BREAK_PERIOD, "9:25");
+      addPeriod(thisMonday, 104, Period.PASSING_PERIOD, "9:40");
+      addPeriod(thisMonday, 4, Period.CLASS_PERIOD, "9:45");
       addPeriod(thisMonday, 300, Period.TUTORIAL_PERIOD, "10:25");
-      addPeriod(thisMonday, 105, Period.PASSING_PERIOD,  "10:55");
-      addPeriod(thisMonday, 5,   Period.CLASS_PERIOD,    "11:00");
-      addPeriod(thisMonday, 106, Period.LUNCH_PERIOD,    "11:40");
-      addPeriod(thisMonday, 106, Period.PASSING_PERIOD,  "12:10");
-      addPeriod(thisMonday, 6,   Period.CLASS_PERIOD,    "12:15");
-      addPeriod(thisMonday, 107, Period.PASSING_PERIOD,  "12:55");
-      addPeriod(thisMonday, 7,   Period.CLASS_PERIOD,    "13:00");
-      addPeriod(thisMonday, 0,   Period.NO_PERIOD,       "13:40");
-
+      addPeriod(thisMonday, 105, Period.PASSING_PERIOD, "10:55");
+      addPeriod(thisMonday, 5, Period.CLASS_PERIOD, "11:00");
+      addPeriod(thisMonday, 106, Period.LUNCH_PERIOD, "11:40");
+      addPeriod(thisMonday, 106, Period.PASSING_PERIOD, "12:10");
+      addPeriod(thisMonday, 6, Period.CLASS_PERIOD, "12:15");
+      addPeriod(thisMonday, 107, Period.PASSING_PERIOD, "12:55");
+      addPeriod(thisMonday, 7, Period.CLASS_PERIOD, "13:00");
+      addPeriod(thisMonday, 0, Period.NO_PERIOD, "13:40");
 
       // S Day
       LocalDate thisTuesday = tuesday.plusWeeks(week);
 
       addPeriod(thisTuesday, 101, Period.PASSING_PERIOD, "7:10");
-      addPeriod(thisTuesday, 1,   Period.CLASS_PERIOD,   "7:15");
-      addPeriod(thisTuesday, 203, Period.BREAK_PERIOD,   "8:55");
+      addPeriod(thisTuesday, 1, Period.CLASS_PERIOD, "7:15");
+      addPeriod(thisTuesday, 203, Period.BREAK_PERIOD, "8:55");
       addPeriod(thisTuesday, 103, Period.PASSING_PERIOD, "9:10");
-      addPeriod(thisTuesday, 3,   Period.CLASS_PERIOD,   "9:15");
-      addPeriod(thisTuesday, 205, Period.BREAK_PERIOD,   "10:55");
+      addPeriod(thisTuesday, 3, Period.CLASS_PERIOD, "9:15");
+      addPeriod(thisTuesday, 205, Period.BREAK_PERIOD, "10:55");
       addPeriod(thisTuesday, 105, Period.PASSING_PERIOD, "11:10");
-      addPeriod(thisTuesday, 5,   Period.CLASS_PERIOD,   "11:15");
-      addPeriod(thisTuesday, 207, Period.LUNCH_PERIOD,   "12:55");
+      addPeriod(thisTuesday, 5, Period.CLASS_PERIOD, "11:15");
+      addPeriod(thisTuesday, 207, Period.LUNCH_PERIOD, "12:55");
       addPeriod(thisTuesday, 107, Period.PASSING_PERIOD, "13:25");
-      addPeriod(thisTuesday, 7,   Period.CLASS_PERIOD,   "13:30");
-      addPeriod(thisTuesday, 0,   Period.NO_PERIOD,      "15:10");
+      addPeriod(thisTuesday, 7, Period.CLASS_PERIOD, "13:30");
+      addPeriod(thisTuesday, 0, Period.NO_PERIOD, "15:10");
 
       LocalDate thisThursday = thursday.plusWeeks(week);
 
       addPeriod(thisThursday, 101, Period.PASSING_PERIOD, "7:10");
-      addPeriod(thisThursday, 1,   Period.CLASS_PERIOD,   "7:15");
-      addPeriod(thisThursday, 203, Period.BREAK_PERIOD,   "8:55");
+      addPeriod(thisThursday, 1, Period.CLASS_PERIOD, "7:15");
+      addPeriod(thisThursday, 203, Period.BREAK_PERIOD, "8:55");
       addPeriod(thisThursday, 103, Period.PASSING_PERIOD, "9:10");
-      addPeriod(thisThursday, 3,   Period.CLASS_PERIOD,   "9:15");
-      addPeriod(thisThursday, 205, Period.BREAK_PERIOD,   "10:55");
+      addPeriod(thisThursday, 3, Period.CLASS_PERIOD, "9:15");
+      addPeriod(thisThursday, 205, Period.BREAK_PERIOD, "10:55");
       addPeriod(thisThursday, 105, Period.PASSING_PERIOD, "11:10");
-      addPeriod(thisThursday, 5,   Period.CLASS_PERIOD,   "11:15");
-      addPeriod(thisThursday, 207, Period.LUNCH_PERIOD,   "12:55");
+      addPeriod(thisThursday, 5, Period.CLASS_PERIOD, "11:15");
+      addPeriod(thisThursday, 207, Period.LUNCH_PERIOD, "12:55");
       addPeriod(thisThursday, 107, Period.PASSING_PERIOD, "13:25");
-      addPeriod(thisThursday, 7,   Period.CLASS_PERIOD,   "13:30");
-      addPeriod(thisThursday, 0,   Period.NO_PERIOD,      "15:10");
+      addPeriod(thisThursday, 7, Period.CLASS_PERIOD, "13:30");
+      addPeriod(thisThursday, 0, Period.NO_PERIOD, "15:10");
 
       // T Day
       LocalDate thisWednesday = wednesday.plusWeeks(week);
 
-      addPeriod(thisWednesday, 102, Period.PASSING_PERIOD,  "7:55");
-      addPeriod(thisWednesday, 2,   Period.CLASS_PERIOD,    "8:00");
-      addPeriod(thisWednesday, 204, Period.BREAK_PERIOD,    "9:40");
-      addPeriod(thisWednesday, 104, Period.PASSING_PERIOD,  "9:55");
-      addPeriod(thisWednesday, 4,   Period.CLASS_PERIOD,    "10:00");
+      addPeriod(thisWednesday, 102, Period.PASSING_PERIOD, "7:55");
+      addPeriod(thisWednesday, 2, Period.CLASS_PERIOD, "8:00");
+      addPeriod(thisWednesday, 204, Period.BREAK_PERIOD, "9:40");
+      addPeriod(thisWednesday, 104, Period.PASSING_PERIOD, "9:55");
+      addPeriod(thisWednesday, 4, Period.CLASS_PERIOD, "10:00");
       addPeriod(thisWednesday, 300, Period.TUTORIAL_PERIOD, "11:40");
-      addPeriod(thisWednesday, 206, Period.LUNCH_PERIOD,    "12:30");
-      addPeriod(thisWednesday, 106, Period.PASSING_PERIOD,  "13:00");
-      addPeriod(thisWednesday, 6,   Period.CLASS_PERIOD,    "13:05");
-      addPeriod(thisWednesday, 0,   Period.NO_PERIOD,       "14:45");
+      addPeriod(thisWednesday, 206, Period.LUNCH_PERIOD, "12:30");
+      addPeriod(thisWednesday, 106, Period.PASSING_PERIOD, "13:00");
+      addPeriod(thisWednesday, 6, Period.CLASS_PERIOD, "13:05");
+      addPeriod(thisWednesday, 0, Period.NO_PERIOD, "14:45");
 
       LocalDate thisFriday = friday.plusWeeks(week);
 
-      addPeriod(thisFriday, 102, Period.PASSING_PERIOD,  "7:55");
-      addPeriod(thisFriday, 2,   Period.CLASS_PERIOD,    "8:00");
-      addPeriod(thisFriday, 204, Period.BREAK_PERIOD,    "9:40");
-      addPeriod(thisFriday, 104, Period.PASSING_PERIOD,  "9:55");
-      addPeriod(thisFriday, 4,   Period.CLASS_PERIOD,    "10:00");
+      addPeriod(thisFriday, 102, Period.PASSING_PERIOD, "7:55");
+      addPeriod(thisFriday, 2, Period.CLASS_PERIOD, "8:00");
+      addPeriod(thisFriday, 204, Period.BREAK_PERIOD, "9:40");
+      addPeriod(thisFriday, 104, Period.PASSING_PERIOD, "9:55");
+      addPeriod(thisFriday, 4, Period.CLASS_PERIOD, "10:00");
       addPeriod(thisFriday, 300, Period.TUTORIAL_PERIOD, "11:40");
-      addPeriod(thisFriday, 206, Period.LUNCH_PERIOD,    "12:30");
-      addPeriod(thisFriday, 106, Period.PASSING_PERIOD,  "13:00");
-      addPeriod(thisFriday, 6,   Period.CLASS_PERIOD,    "13:05");
-      addPeriod(thisFriday, 0,   Period.NO_PERIOD,       "14:45");
+      addPeriod(thisFriday, 206, Period.LUNCH_PERIOD, "12:30");
+      addPeriod(thisFriday, 106, Period.PASSING_PERIOD, "13:00");
+      addPeriod(thisFriday, 6, Period.CLASS_PERIOD, "13:05");
+      addPeriod(thisFriday, 0, Period.NO_PERIOD, "14:45");
     }
     return OK;
   }
@@ -249,18 +256,15 @@ public class DevelopmentController {
     Period period = new Period();
     period.number = number;
     period.type = type;
-    period.startTime =
-        day.atTime(Utils.parseInteger(startComponents[0]), Utils.parseInteger(startComponents[1]))
-            .atZone(Utils.TIMEZONE)
-            .toInstant()
-            .toEpochMilli();
+    period.startTime = day.atTime(Utils.parseInteger(startComponents[0]), Utils.parseInteger(startComponents[1]))
+        .atZone(Utils.TIMEZONE).toInstant().toEpochMilli();
     periodService.add(period);
   }
 
   @RequestMapping("/populateSemesters")
   public ResponseEntity<?> populateSemesters() {
     semesterService.deleteAll();
-    for(int year = 2018; year < 2023; year++) {
+    for (int year = 2018; year < 2023; year++) {
       addSemester(Semester.SPRING_SEMESTER, LocalDate.of(year, 1, 1));
       addSemester(Semester.SUMMER_SEMESTER, LocalDate.of(year, 6, 1));
       addSemester(Semester.FALL_SEMESTER, LocalDate.of(year, 8, 1));
@@ -270,11 +274,7 @@ public class DevelopmentController {
 
   void addSemester(String type, LocalDate date) {
     Semester semester = new Semester();
-    semester.startTime = date
-      .atTime(0,0)
-      .atZone(Utils.TIMEZONE)
-      .toInstant()
-      .toEpochMilli();
+    semester.startTime = date.atTime(0, 0).atZone(Utils.TIMEZONE).toInstant().toEpochMilli();
     semester.year = date.getYear();
     semester.type = type;
 
