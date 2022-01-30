ba87999862cebebb8094f6385666f2b9d06b42c3
gpi <gpimpale29@gmail.com>
2022-01-29 16:05:40 -0800

    java run on 7080

--- scripts/deploy-java.sh
+++ scripts/deploy-java.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 
-set -e
-echo Please enter password for innexgo.com key below:
-read -s PASSWORD
-
 java -jar \
-	-Dserver.port=8443 \
-	-Dserver.ssl.enabled=true \
-	-Dserver.ssl.key-store=/etc/letsencrypt/live/innexgo.com/keystore.p12 \
-	-Dserver.ssl.key-store-password=$PASSWORD \
-	-Dserver.ssl.keyStoreType=PKCS12 \
-	-Dserver.ssl.keyAlias=tomcat \
+	-Dserver.port=7080 \
 	./build/libs/innexgo-0.0.1-SNAPSHOT.jar > ~/logs.txt &
 
--- scripts/deploy-node.sh
+++
@@ -1,9 +0,0 @@
-:
-#!/bin/bash
-
-set -e
-
-echo Starting the 8080 to 8443 node server
-cd ./redirectSecureServer
-node app.js &
-cd ..
